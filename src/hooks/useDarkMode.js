7b20bca72cca3437f8349853e8ebf2e8fc752f14
BenHall-7 <benjaminjahall@gmail.com>
2020-01-21 22:51:54 -0500

    Fix exports and import DarkMode to page

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,11 +1,13 @@
 import React, {useState, useEffect} from 'react';
 import useLocalStorage from './useLocalStorage';
 
-export default useDarkMode = () => {
-    const [darkMode, setDarkMode] = useLocalStorage("isDarkMode", false);
+const useDarkMode = (initial) => {
+    const [darkMode, setDarkMode] = useLocalStorage("isDarkMode", initial);
     useEffect(() => {
         let body = document.querySelector('body');
         darkMode ? body.classList.add('dark-mode') : body.classList.remove('dark-mode');
     }, [darkMode]);
     return [darkMode, setDarkMode]
 }
+
+export default useDarkMode;
